e9c72290fd838ab11d7fac47f1b02c5ca014c89f
砸你家玻璃 <tim_duncan@sogou.com>
2016-10-13 23:53:56 +0800

    ini

--- src/main/java/com/ww/actions/Example.java
+++ src/main/java/com/ww/actions/Example.java
@@ -1,6 +1,5 @@
 package com.ww.actions;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +12,4 @@ public class Example {
 		return "Hello World!";
 	}
 
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(Example.class, args);
-	}
 }
