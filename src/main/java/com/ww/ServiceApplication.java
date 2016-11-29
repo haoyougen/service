bfcdc167261b55532b160b0cab75dcd376d8142b
wangshuguang <wangshuguang@rongcapital.cn>
2016-11-29 17:15:01 +0800

    dd

--- src/main/java/com/ww/ServiceApplication.java
+++ src/main/java/com/ww/ServiceApplication.java
@@ -2,19 +2,23 @@ package com.ww;
 
 import java.io.File;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @ImportResource(value = { "classpath:service-web.xml" })
+@EnableAutoConfiguration
 public class ServiceApplication {
+	private static final Logger LOGGER = LoggerFactory.getLogger(ServiceApplication.class);
 
 	public static void main(String[] args) {
 		// logback.configurationFile
 		System.setProperty("logging.config", System.getProperty("APP_HOME") + File.separator + "conf" + File.separator
 				+ System.getProperty("env") + File.separator + "logback.xml");
-
 		SpringApplication.run(ServiceApplication.class, args);
 	}
 }
